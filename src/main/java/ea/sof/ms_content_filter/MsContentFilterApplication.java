5b68c0936038e71d4d92be60cf7c89cbdda0ecb9
rustem bayetov <rustem.bayetov@gmail.com>
2019-11-17 16:29:02 -0600

    init commit

---
+++ src/main/java/ea/sof/ms_content_filter/MsContentFilterApplication.java
@@ -0,0 +1,13 @@
+package ea.sof.ms_content_filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsContentFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsContentFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ea/sof/ms_content_filter/MsContentFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package ea.sof.ms_content_filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsContentFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
