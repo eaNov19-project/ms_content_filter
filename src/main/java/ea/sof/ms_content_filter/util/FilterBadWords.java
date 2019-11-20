5590fa718d1067d6ec5371ac2912feb58eb001c6
thutruongvn <thutruong.1986@gmail.com>
2019-11-20 10:26:18 -0600

    fixed the stupid issue of reading file

--- src/main/java/ea/sof/ms_content_filter/util/FilterBadWords.java
+++ src/main/java/ea/sof/ms_content_filter/util/FilterBadWords.java
@@ -2,24 +2,55 @@ package ea.sof.ms_content_filter.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+//import javax.annotation.Resource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class FilterBadWords {
-    public static final String BAD_WORD_FILE_PATH = "config/bad_words.txt";
+    public static final String BAD_WORD_FILE_PATH = "bad_words.txt";
     private static final Logger LOGGER = LoggerFactory.getLogger(FilterBadWords.class);
+//    @Value("classpath:bad_words.txt")
+//    Resource resourceFile;
+    public Resource loadRes() {
+        return new ClassPathResource(BAD_WORD_FILE_PATH);
+    }
     public boolean filterBadWords(String content)  {
+//        System.out.println(this.resourceFile);
+        BufferedReader reader = null;
         try {
-            Scanner scanner = new Scanner(new File(getClass().getClassLoader().getResource(BAD_WORD_FILE_PATH).getFile()));
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                if (content.contains(line)) {
+//            InputStream is = new FileInputStream(resourceFile.name());
+            Resource res = loadRes();
+            System.out.println(res);
+//            FileInputStream is = (FileInputStream) res.getInputStream();
+//            reader = new BufferedReader(new InputStreamReader(is));
+            reader = new BufferedReader(
+                    new InputStreamReader(res.getInputStream(), StandardCharsets.UTF_8));
+
+            System.out.println("Reading File line by line using BufferedReader");
+
+            String line = reader.readLine();
+            while(line != null){
+                System.out.println(line);
+                if (content.toLowerCase().contains(line.trim())) {
                     return true;
                 }
+                line = reader.readLine();
             }
-        }catch (FileNotFoundException ex){
+
+//            Scanner scanner = new Scanner(res.getFile());
+//            while (scanner.hasNextLine()) {
+//                String line = scanner.nextLine();
+//                if (content.contains(line)) {
+//                    return true;
+//                }
+//            }
+        }catch (IOException ex){
             System.out.println(ex.getMessage());
             LOGGER.error("FilterBadWords error: " + ex.getMessage());
         }
